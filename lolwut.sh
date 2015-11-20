50a34196e68cce3f0e4ffc9f1c2c7c73e556dc88
Dave Havlicek <xens@comcast.net>
2015-11-20 16:54:46 -0600

    prank 1

---
+++ lolwut.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+brew install npm
+npm install alex --global
+git config --global init.templatedir '~/.git-templates'
+mkdir -p ~/.git-templates/hooks
+cat > ~/.git-templates/hooks/commit-msg <<- EOM
+#!/bin/bash
+
+ALEX=\$(cat "\$1" | alex --color)
+if [ \$? -eq 0 ]
+then
+  exit 0
+else
+  echo "\$ALEX"
+  exit 1
+fi
+EOM
+chmod +x ~/.git-templates/hooks/commit-msg
+find ~/ -type d -name .git -exec sh -c "cd \"{}\"/../ && git init" \;
