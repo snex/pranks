4cbfe98389a77c1c1ab53f2c7d15dcfc93e97dc3
Dave Havlicek <dhavlicek@enova.com>
2015-11-20 18:36:17 -0600

    be more verbose

--- lolwut.sh
+++ lolwut.sh
@@ -12,8 +12,10 @@ if [ \$? -eq 0 ]
 then
   exit 0
 else
-  echo \$(cat "\$1")
+  echo "There was a problem with your commit message: \$(cat "\$1")"
   echo "\$ALEX"
+  echo ""
+  echo "Please edit your commit with 'git commit --amend' and try again."
   exit 1
 fi
 EOM
